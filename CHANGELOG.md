c2358daf2923c0ea537da6a5e7de616d078c53b1
Aaron <qzh6662@gmail.com>
2021-02-12 15:31:58 +0800

    init

---
+++ CHANGELOG.md
@@ -0,0 +1,79 @@
+## <small>1.7.3 (2021-01-16)</small>
+
+* fix: fix github readme render error ([a3df62e](https://github.com/sinlov/sinlov/commit/a3df62e))
+
+## <small>1.7.2 (2021-01-16)</small>
+
+* docs: add title of waka time record ([3794103](https://github.com/sinlov/sinlov/commit/3794103))
+
+## <small>1.7.1 (2021-01-16)</small>
+
+* fix: fix weather information not write success ([59c195a](https://github.com/sinlov/sinlov/commit/59c195a))
+
+## 1.7.0 (2021-01-16)
+
+* try to fix writeKeepData error ([172df94](https://github.com/sinlov/sinlov/commit/172df94))
+
+## <small>1.6.2 (2021-01-16)</small>
+
+* add diff show of README.md to find bug at github action build ([c0c58e9](https://github.com/sinlov/sinlov/commit/c0c58e9))
+* change git commit way of info ([b7e066f](https://github.com/sinlov/sinlov/commit/b7e066f))
+* print time different to find out error ([9d357fc](https://github.com/sinlov/sinlov/commit/9d357fc))
+* show diff README.md ([1a3c644](https://github.com/sinlov/sinlov/commit/1a3c644))
+* feat: change main-publish use ad-m/github-push-action ([f16972d](https://github.com/sinlov/sinlov/commit/f16972d))
+* feat: fix github-push-action repository set error ([4c56ea0](https://github.com/sinlov/sinlov/commit/4c56ea0))
+* feat: let keepWakaOldData fullContent write as async ([c90f741](https://github.com/sinlov/sinlov/commit/c90f741))
+* feat: print time of build to find bug ([73e9b5a](https://github.com/sinlov/sinlov/commit/73e9b5a))
+
+## <small>1.6.1 (2021-01-15)</small>
+
+* open log at keepWakaOldData ([eedf1cc](https://github.com/sinlov/sinlov/commit/eedf1cc))
+
+## 1.6.0 (2021-01-14)
+
+* feat: add keepReadMeData to keep README old data ([d181faa](https://github.com/sinlov/sinlov/commit/d181faa))
+
+## <small>1.5.1 (2021-01-12)</small>
+
+* ci: change waka-readme schedule cron set to 01:00 on Monday ([a4fd0c3](https://github.com/sinlov/sinlov/commit/a4fd0c3))
+
+## 1.5.0 (2021-01-06)
+
+* fix: fix waka-readme.yml build step error ([1d6f0da](https://github.com/sinlov/sinlov/commit/1d6f0da))
+
+## 1.4.0 (2021-01-05)
+
+* feat: change workflow to different cron set for update README.md ([fa7042f](https://github.com/sinlov/sinlov/commit/fa7042f))
+
+## 1.3.0 (2021-01-03)
+
+* feat: add wakatime at workflow ([004373f](https://github.com/sinlov/sinlov/commit/004373f))
+
+## 1.2.0 (2020-12-31)
+
+* feat: add show_travis config to show build status and remove badge of travis ([9e5ed64](https://github.com/sinlov/sinlov/commit/9e5ed64))
+
+## 1.1.0 (2020-12-31)
+
+* add weather data cache ([ba2ec9f](https://github.com/sinlov/sinlov/commit/ba2ec9f))
+* filter config Separate configuration files and add check of config ([de55e3a](https://github.com/sinlov/sinlov/commit/de55e3a))
+
+
+## 1.0.0 (2020-12-30)
+
+* feat: add city feels like temperature ([2315eda](https://github.com/sinlov/sinlov/commit/2315eda))
+* feat: add missing env COMMIT_MSG ([b3c158e](https://github.com/sinlov/sinlov/commit/b3c158e))
+* feat: add new cat image ([3042e69](https://github.com/sinlov/sinlov/commit/3042e69))
+* feat: add switchThemeByTime for different time of show status ([1f2c0fa](https://github.com/sinlov/sinlov/commit/1f2c0fa))
+* feat: add travis-ci build status image link ([a79a391](https://github.com/sinlov/sinlov/commit/a79a391))
+* feat: can use weather api to auto choose show theme of stats ([ee9c484](https://github.com/sinlov/sinlov/commit/ee9c484))
+* feat: change badge info to json and add travis build config ([59b6d6a](https://github.com/sinlov/sinlov/commit/59b6d6a))
+* feat: let switchThemeByTime support timezone set ([e5a9347](https://github.com/sinlov/sinlov/commit/e5a9347))
+* feat: let weather show more things ([b311c22](https://github.com/sinlov/sinlov/commit/b311c22))
+* feat: try to fix GITHUB_TOKEN code ([0add7c3](https://github.com/sinlov/sinlov/commit/0add7c3))
+* feat: update main-publish.yml GITHUB_TOKEN get and try to push new READM.md ([77f818a](https://github.com/sinlov/sinlov/commit/77f818a))
+* add build status of page bottom|right ([898714d](https://github.com/sinlov/sinlov/commit/898714d))
+* add env test of github action ([6755e80](https://github.com/sinlov/sinlov/commit/6755e80))
+* Create main-publish.yml ([24204c4](https://github.com/sinlov/sinlov/commit/24204c4))
+* first commit ([d11a846](https://github.com/sinlov/sinlov/commit/d11a846))
+* update travis script for cache npm ([5f80818](https://github.com/sinlov/sinlov/commit/5f80818))
---
+++ index.js
@@ -0,0 +1,391 @@
+require('dotenv').config();
+
+const fs = require('fs');
+const fs_path = require('path');
+const extend = require('extend');
+const querystring = require('querystring');
+const fetch = require('node-fetch');
+const Mustache = require('mustache');
+const moment = require('moment-timezone');
+const fsUtils = require("nodejs-fs-utils");
+const lineByLine = require('n-readlines');
+// const lineReader = require('line-reader');
+// const sleep = require('sleep');
+
+function isDebug() {
+    return process.env.RUN_MODE && process.env.RUN_MODE !== 'prod'
+}
+
+/**
+ * default setting at here
+ */
+let DATA = {
+    branch: 'main',
+    lang: 'en',
+    i18n_name: 'en-US',
+    time_zone: 'Asia/Shanghai',
+    units: 'metric',
+    wind_speed_units: 'meter/sec',
+    git_action_main_publish_name: 'build-README.md',
+    readme_cache: {
+        waka: [],
+    },
+    show_travis: false,
+    code_with_list: []
+};
+
+/**
+ * DATA is the object that contains all
+ * the data to be provided to Mustache.
+ */
+const MUSTACHE_MAIN_DIR = './main.mustache';
+
+async function mergeConfig() {
+    let code_with_badge_info = JSON.parse(fs.readFileSync('./badge_info.json'));
+    let index_config = JSON.parse(fs.readFileSync('./index.json'));
+    extend(DATA, index_config);
+    DATA.code_with_badge = code_with_badge_info;
+    DATA.date = new Date().toLocaleDateString(DATA.i18n_name, {
+        weekday: 'long',
+        month: 'long',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        timeZoneName: 'short',
+        timeZone: DATA.time_zone
+    })
+
+    if (process.env.GITHUB_ACTOR) {
+        DATA.name = process.env.GITHUB_ACTOR
+    }
+
+    if (process.env.CITY_NAME) {
+        DATA.city = process.env.CITY_NAME
+    }
+
+    if (isDebug()) {
+        console.log('merge DATA: ', JSON.stringify(DATA));
+    }
+
+    if (!DATA.name) {
+        console.log(new Error('error: index.json not set name, or not set env GITHUB_ACTOR, this env will auto include when run as github action.'));
+        process.exit(128);
+    }
+
+    if (!DATA.city) {
+        console.log(new Error('error: index.json not set city, or not set env CITY_NAME'));
+        process.exit(128);
+    }
+    if (!process.env.OPEN_WEATHER_MAP_KEY) {
+        console.log('Warning not set env OPEN_WEATHER_MAP_KEY , please add as https://home.openweathermap.org/api_keys');
+    }
+}
+
+/**
+ * https://img.shields.io/ get image of badge
+ * @param {*.bg_color} icon background
+ * @param {*.logo} logo https://simpleicons.org/?q={name}
+ */
+function urlShieldBadge(info) {
+    let defaultInfo = {
+        label: 'Go',
+        bg_color: '00ACD7',
+        logo: 'go',
+        logoColor: 'white',
+        style: 'flat-square'
+    };
+    extend(defaultInfo, info);
+    info.bg_color = info.bg_color.replace('#', '');
+    info.logoColor = info.logoColor.replace('#', '');
+    return `https://img.shields.io/badge/-${info.label}-${info.bg_color}?${querystring.stringify(
+        {
+            logo: info.logo,
+            logoColor: info.logoColor,
+            style: info.style
+        }
+    )}`
+}
+
+function shieldBadgeHtml(info) {
+    let defaultHtmlInfo = {
+        label: 'Go',
+    };
+    extend(defaultHtmlInfo, info);
+    if (info.link) {
+        return `<a href="${info.link}"><img alt="${info.label}" src="${urlShieldBadge(info)}"/></a>`
+    }
+    return `<img alt="${info.label}" src="${urlShieldBadge(info)}"/>`
+}
+
+/**
+ * https://img.shields.io/ get image of static
+ * @param {*.color} icon background
+ * @param {*.logo} logo https://simpleicons.org/?q={name}
+ * @param {*.logoColor} logo color
+ */
+function urlShieldStatic(info) {
+    let defaultInfo = {
+        label: 'Go',
+        message: 'golang',
+        color: '273238',
+        logo: 'go',
+        logoColor: '00ACD7',
+        style: 'flat-square'
+    };
+    extend(defaultInfo, info);
+    // delete info.link
+    info.color = info.color.replace('#', '');
+    info.logoColor = info.logoColor.replace('#', '');
+    return `https://img.shields.io/static/v1?${querystring.stringify(info)}`
+}
+
+function shieldStaticHTML(info) {
+    let defaultInfo = {
+        label: 'Go',
+    };
+    extend(defaultInfo, info);
+    if (info.link) {
+        return `<a href="${info.link}"><img alt="${info.label}" src="${urlShieldStatic(info)}"/></a>`
+    }
+    return `<img alt="${info.label}" src="${urlShieldStatic(info)}"/>`
+}
+
+
+async function getImgShields() {
+    if (DATA.code_with_badge) {
+        for (let i in DATA.code_with_badge) {
+            DATA.code_with_list.push(shieldBadgeHtml(DATA.code_with_badge[i]))
+        }
+    }
+}
+
+async function cacheWakaOldData() {
+
+    let liner = new lineByLine('README.md');
+    let line;
+    let nextCache = false;
+    while (line = liner.next()) {
+        if (line.toString().indexOf(`<!--END_SECTION:waka-->`) !== -1) {
+            nextCache = false;
+        }
+        if (nextCache) {
+            DATA.readme_cache.waka.push(`${line}`);
+        }
+        if (line.toString().indexOf(`<!--START_SECTION:waka-->`) !== -1) {
+            nextCache = true;
+        }
+    }
+
+    // let nextCache = false;
+    // lineReader.eachLine('README.md', (line, last) => {
+    //     if (last){
+    //         console.log(`cacheWakaOldData DATA.readme_cache.waka:\n${DATA.readme_cache.waka.join('\n')}`);
+    //     }
+    //     if (line.toString().indexOf(`<!--END_SECTION:waka-->`) !== -1) {
+    //         nextCache = false;
+    //     }
+    //     if (nextCache) {
+    //         DATA.readme_cache.waka.push(`${line}`);
+    //     }
+    //     if (line.toString().indexOf(`<!--START_SECTION:waka-->`) !== -1) {
+    //         nextCache = true;
+    //     }
+    // });
+}
+
+async function keepReadMeData() {
+    if (isDebug()) {
+        console.log('keepReadMeData start');
+    }
+    cacheWakaOldData();
+    if (isDebug()) {
+        console.log('keepReadMeData end');
+    }
+    /**
+     * Fetch Weather
+     * must load env as OPEN_WEATHER_MAP_KEY
+     */
+    if (process.env.OPEN_WEATHER_MAP_KEY) {
+        setWeatherInformation(DATA.city, process.env.OPEN_WEATHER_MAP_KEY, DATA.lang, DATA.units);
+    } else {
+        console.log('warn: pass weather generate');
+        switchThemeByTime(DATA.time_zone, DATA.sun_rise_timestamp, DATA.sun_set_timestamp);
+        generateReadMe();
+    }
+}
+
+/**
+ * API from https://openweathermap.org/current
+ * @param {*} city all by city name or city name, state code and country code. Please note that searching by states available only for the USA locations.
+ * @param {*} appid see: https://home.openweathermap.org/api_keys
+ * @param {*} lang see: https://openweathermap.org/current#multi
+ * @param {*} units see: https://openweathermap.org/current#data
+ */
+async function setWeatherInformation(city, appid, lang, units) {
+    if (!appid) {
+        return
+    }
+
+    async function save_weather_data(data) {
+        let date_f = moment().tz(DATA.time_zone);
+        let saveDir = fs_path.resolve(fs_path.join('.', 'weather_data', date_f.format('yyyy'), date_f.format('MM'), date_f.format('DD')));
+        if (!fs.existsSync(saveDir)) {
+            // console.log(`saveDir ennd mkdir ${saveDir}`);
+            fsUtils.mkdirsSync(saveDir);
+        }
+        let saveFile = fs_path.join(saveDir, `weather-data-${date_f.format('yyyy')}-${date_f.format('MM')}-${date_f.format('DD')}.json`);
+        // console.log(saveFile);
+        let saveData = [];
+        if (fs.existsSync(saveFile)) {
+            let old = JSON.parse(fs.readFileSync(saveFile));
+            for (i in old) {
+                saveData.push(old[i]);
+            }
+        }
+        saveData.push({
+            date: moment().format(),
+            weather: data
+        });
+        fs.writeFileSync(saveFile, JSON.stringify(saveData, null, 2));
+    }
+
+    let openweathermap_url = `https://api.openweathermap.org/data/2.5/weather?${querystring.stringify({q: city})}&appid=${appid}&lang=${lang}&units=${units}`
+    await fetch(openweathermap_url)
+        .then(r => r.json())
+        .then(r => {
+            DATA.city_temperature = r.main.temp.toFixed(1);
+            DATA.city_feels_like = r.main.feels_like.toFixed(1);
+            DATA.city_humidity = r.main.humidity;
+            DATA.city_weather_icon = `http://openweathermap.org/img/wn/${r.weather[0].icon}@2x.png`;
+            DATA.city_weather = r.weather[0].description;
+            DATA.wind_speed = r.wind.speed;
+            DATA.sun_rise_timestamp = r.sys.sunrise * 1000;
+            DATA.sun_rise = new Date(r.sys.sunrise * 1000).toLocaleString(DATA.i18n_name, {
+                hour: '2-digit',
+                minute: '2-digit',
+                timeZone: DATA.time_zone,
+            });
+            DATA.sun_set_timestamp = r.sys.sunset * 1000;
+            DATA.sun_set = new Date(r.sys.sunset * 1000).toLocaleString(DATA.i18n_name, {
+                hour: '2-digit',
+                minute: '2-digit',
+                timeZone: DATA.time_zone,
+            });
+            if (!isDebug()) {
+                save_weather_data(r);
+            }
+            /**
+             * sit different theme of status
+             */
+            switchThemeByTime(DATA.time_zone, DATA.sun_rise_timestamp, DATA.sun_set_timestamp);
+            generateReadMe();
+        });
+}
+
+async function switchThemeByTime(timezone = "America/Los_Angeles", sun_rise_timestamp = 0, sun_set_timestamp = 0) {
+    let sun_rise = 7;
+    if (sun_rise_timestamp != 0) {
+        sun_rise = Number(moment(sun_rise_timestamp).tz(timezone).format('H'));
+    }
+    let sun_set = 22;
+    if (sun_set_timestamp != 0) {
+        sun_set = Number(moment(sun_set_timestamp).tz(timezone).format('H'));
+    }
+    let currentHour = moment().tz(timezone).format('H');
+    // console.log('sun_rise', sun_rise, 'sun_set', sun_set, 'currentHour', currentHour, currentHour >= sun_set, currentHour < sun_rise);
+    let nightModeApplicable = (currentHour >= sun_set || currentHour < sun_rise) ? true : false;
+    if (nightModeApplicable) {
+        DATA.github_readme_stats_url = `https://github-readme-stats.vercel.app/api?username=${DATA.name}&show_icons=true&theme=dracula`
+    } else {
+        DATA.github_readme_stats_url = `https://github-readme-stats.vercel.app/api?username=${DATA.name}&show_icons=true&theme=buefy`
+    }
+}
+
+async function generateReadMe() {
+    if (isDebug()) {
+        console.log('generateReadMe start');
+    }
+    fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
+        if (err) throw err;
+        const output = Mustache.render(data.toString(), DATA);
+        // fs.writeFileSync('README.md', output);
+        writeKeepData('README.md', output);
+    });
+    if (isDebug()) {
+        console.log('generateReadMe finish');
+    }
+}
+
+async function writeKeepData(path, content) {
+    console.log(`writeKeepData DATA.readme_cache.waka:\n${DATA.readme_cache.waka.join('\n')}`);
+    if (DATA.readme_cache.waka.length !== 0) {
+        let fullContent = []
+        let writeCache = false;
+        if (isDebug()) {
+            console.log(`writeKeepData content:\n${content}`);
+        }
+        let oldContent = content.toString().split('\n');
+        oldContent.forEach(line => {
+            fullContent.push(line.toString());
+            if (line.toString().indexOf(`<!--START_SECTION:waka-->`) !== -1) {
+                writeCache = true;
+            }
+            if (writeCache) {
+                // console.log(`add DATA.readme_cache.waka:\n${DATA.readme_cache.waka.join('\n')}`);
+                fullContent.push(DATA.readme_cache.waka.join('\n'));
+                writeCache = false;
+            }
+        });
+        // let liner = new lineByLine(path);
+        // let line;
+        // while (line = liner.next()) {
+        //     fullContent.push(line.toString());
+        //     if (line.toString().indexOf(`<!--START_SECTION:waka-->`) !== -1) {
+        //         writeCache = true;
+        //     }
+        //     if (writeCache) {
+        //         console.log(`add DATA.readme_cache.waka:\n${DATA.readme_cache.waka.join('\n')}`);
+        //         fullContent.push(DATA.readme_cache.waka.join('\n'));
+        //         writeCache = false;
+        //     }
+        // }
+
+        fullReadme = fullContent.join('\n');
+        if (isDebug()) {
+            console.log(`write fullContent:\n${fullReadme}`);
+        }
+        fs.writeFileSync(path, fullReadme);
+    }
+}
+
+// async function setInstagramPosts() {
+//   const instagramImages = await puppeteerService.getLatestInstagramPostsFromAccount('visitstockholm', 3);
+//   DATA.img1 = instagramImages[0];
+//   DATA.img2 = instagramImages[1];
+//   DATA.img3 = instagramImages[2];
+// }
+
+/**
+ * do github action
+ */
+async function action() {
+
+    /**
+     * first merge config
+     */
+    await mergeConfig();
+
+    await getImgShields();
+
+    await keepReadMeData();
+
+    // });
+    /**
+     * future 👋
+     */
+    // await puppeteerService.close();
+}
+
+action().then(r => {
+
+});
